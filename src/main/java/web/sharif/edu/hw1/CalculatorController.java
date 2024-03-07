041adc48864db536f1371830177b24812cfbd008
Muhammad Khosravi <mohamad138121@gmail.com>
2024-03-07 12:03:46 +0330

    initial commit

---
+++ src/main/java/web/sharif/edu/hw1/CalculatorController.java
@@ -0,0 +1,24 @@
+package web.sharif.edu.hw1;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    @GetMapping("/calc/{operator}/{op1}/{op2}")
+    public String calc(@PathVariable String operator, @PathVariable String op1, @PathVariable String op2) {
+        int int_op1 = Integer.parseInt(op1);
+        int int_op2 = Integer.parseInt(op2);
+
+        return switch (operator) {
+            case "add" -> int_op1 + " + " + int_op2 + " = " + (int_op1 + int_op2);
+            case "sub" -> int_op1 + " - " + int_op2 + " = " + (int_op1 - int_op2);
+            case "mult" -> int_op1 + " * " + int_op2 + " = " + (int_op1 * int_op2);
+            case "div" -> int_op1 + " / " + int_op2 + " = " + ((float) int_op1 / (float) int_op2);
+            default -> "Invalid operator";
+        };
+    }
+
+}
